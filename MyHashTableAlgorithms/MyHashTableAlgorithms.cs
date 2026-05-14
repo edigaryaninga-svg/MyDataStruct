f93b5d2f70e6353eeb0858f96f58fd64cdab7f4c
edigaryaninga-svg <edigaryaninga@gmail.com>
2026-05-14 16:37:38 +0400

    Add files via upload

---
+++ MyHashTableAlgorithms/MyHashTableAlgorithms.cs
@@ -0,0 +1,70 @@
+﻿namespace MyHashTableAlgorithms;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        GetNextBytes(startIndex: 0, "lore");
+        string input = string.Empty;
+
+        while (!input.Equals("quit", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.Write("> ");
+            Console.WriteLine("Folding: {0}", FoldingHash(input));
+        }
+    }
+
+    public static int AdditiveHash(string input)
+    {
+        int currentHash = 0;
+        foreach (char c in input)
+        {
+            unchecked
+            {
+                currentHash += (int)c;
+            }
+        }
+        return currentHash;
+    }
+
+    public static int FoldingHash(string input)
+    {
+        int hashValue = 0;
+
+        int startIndex = 0;
+        int currentFourBytes;
+
+        do
+        {
+            currentFourBytes = GetNextBytes(startIndex, input);
+            unchecked
+            {
+                hashValue += currentFourBytes;
+            }
+            startIndex += 4;
+        } while (currentFourBytes != 0);
+
+        return hashValue;
+    }
+
+    public static int GetNextBytes(int startIndex, string str)
+    {
+        int currentFourBytes = 0;
+
+        currentFourBytes += GetByte(str, startIndex);
+        currentFourBytes += GetByte(str, index: startIndex + 1) << 8;
+        currentFourBytes += GetByte(str, index: startIndex + 2) << 16;
+        currentFourBytes += GetByte(str, index: startIndex + 3) << 24;
+
+        return currentFourBytes;
+    }
+
+    public static int GetByte(string str, int index)
+    {
+        if (index < str.Length)
+        {
+            return (int)str[index];
+        }
+        return 0;
+    }
+}
---
+++ MyQueueProj/MyQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MyLinkedListProj; 
+
+namespace MyQueueProj
+{
+    public class MyQueue<T> : IEnumerable<T>
+    {
+       
+        private MyLinkedList<T> _items = new MyLinkedList<T>();
+
+        public void Enqueue(T value)
+        {
+            _items.AddLast(value);
+        }
+
+        public T Dequeue()
+        {
+            if (_items.Count == 0) return default(T);
+
+          
+            T value = _items.Head.Value;
+            _items.RemoveFirst();
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (_items.Head == null) return default(T);
+            return _items.Head.Value;
+        }
+
+        public int Count => _items.Count;
+
+        public void Clear() => _items.Clear();
+
+        public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
